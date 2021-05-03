65d75c44f506c6a9ed8e8c4a289053b552665739
starcn00 <kami123.zip@gmail.com>
2021-05-03 10:34:13 +0800

    Create trojan-go.sh

---
+++ trojan-go.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -euo pipefail
+
+function prompt() {
+    while true; do
+        read -p "$1 [y/N] " yn
+        case $yn in
+            [Yy] ) return 0;;
+            [Nn]|"" ) return 1;;
+        esac
+    done
+}
+
+if [[ $(id -u) != 0 ]]; then
+    echo Please run this script as root.
+    exit 1
+fi
+
+if [[ $(uname -m 2> /dev/null) != x86_64 ]]; then
+    echo Please run this script on x86_64 machine.
+    exit 1
+fi
+
+NAME=trojan-go
+VERSION=$(curl -fsSL https://github.com/p4gefau1t/trojan-go/releases | grep tag_name |awk -F";" '{print $2}' | head -1|sed -E 's/.*=v(.*)\&.*/\1/')
+TARBALL="$NAME-linux-amd64.zip"
+DOWNLOADURL="https://github.com/p4gefau1t/$NAME/releases/download/v$VERSION/$TARBALL"
+TMPDIR="$(mktemp -d)"
+INSTALLPREFIX=/usr/local
+SYSTEMDPREFIX=/etc/systemd/system
+
+BINARYPATH="$INSTALLPREFIX/bin/$NAME"
+CONFIGPATH="$INSTALLPREFIX/etc/$NAME/config.json"
+SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
+
+echo Entering temp directory $TMPDIR...
+cd "$TMPDIR"
+
+echo Downloading $NAME $VERSION...
+curl -LO --progress-bar "$DOWNLOADURL" || wget -q --show-progress "$DOWNLOADURL"
+
+echo Unpacking $NAME $VERSION...
+unzip "$TARBALL"
+
+echo Installing $NAME $VERSION to $BINARYPATH...
+install -Dm755 "$NAME" "$BINARYPATH"
+
+echo Installing $NAME server config to $CONFIGPATH...
+if ! [[ -f "$CONFIGPATH" ]] || prompt "The server config already exists in $CONFIGPATH, overwrite?"; then
+    install -Dm644 examples/server.json-example "$CONFIGPATH"
+else
+    echo Skipping installing $NAME server config...
+fi
+
+if [[ -d "$SYSTEMDPREFIX" ]]; then
+    echo Installing $NAME systemd service to $SYSTEMDPATH...
+    if ! [[ -f "$SYSTEMDPATH" ]] || prompt "The systemd service already exists in $SYSTEMDPATH, overwrite?"; then
+        cat > "$SYSTEMDPATH" << EOF
+[Unit]
+Description=Trojan-Go - An unidentifiable mechanism that helps you bypass GFW
+Documentation=https://p4gefau1t.github.io/trojan-go/
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+StandardError=journal
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart="/usr/local/bin/trojan-go" -config "/usr/local/etc/trojan-go/config.json"
+LimitNOFILE=51200
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+        echo Reloading systemd daemon...
+        systemctl daemon-reload
+    else
+        echo Skipping installing $NAME systemd service...
+    fi
+fi
+
+echo Deleting temp directory $TMPDIR...
+#rm -rf "$TMPDIR"
+
+echo Done!
